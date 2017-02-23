8e42e713782eaa0d5756b97d13a7e25600621d0c
rgscherf <rgscherf@gmail.com>
2017-02-23 12:36:35 -0500

    todo item padding

--- src/tood/core.clj
+++ src/tood/core.clj
@@ -6,51 +6,82 @@
             [lanterna.screen :as s]))
 
 ;; (nrepl/start-nrepl 7855)
-(defonce scr (s/get-screen :swing))
+(defonce screen (s/get-screen :swing))
 
 ;; data store
-(defonce input
+(defonce state
   (atom
    [{:title    "Buy apples"
      :added    (Date. 117 03 26)
      :done     false
      :priority 1}
+    {:title    "Wear birkenstocks!"
+     :added    (Date.)
+     :done     true
+     :priority 2}
     {:title    "Measure pictures"
      :added    (Date. 117 02 13)
      :done     false
      :priority 5}]))
 
-(defn- sorted-input-items
-  []
-  (let [hellos (map str (sort-by :date  @input))]
-    (map (fn [a b] [a b])
-         (range (count hellos))
-         hellos)))
+(defn additional-spaces
+  [num-spaces]
+  (apply str (repeat num-spaces " ")))
 
-(defn print-todos!
+(defn pad-string
+  "Pad a string to intended length from the left (default) or right. If intended length is > the string's length, the string will be truncated."
+  ([string intended-len]
+   (pad-string string intended-len true))
+  ([string intended-len pad-left?]
+   (let [spaces-to-add (- intended-len
+                          (count string))
+         spaces-as-string (additional-spaces spaces-to-add)]
+     (if (neg? spaces-to-add)
+       (apply str (take intended-len string))
+       (if pad-left?
+         (str spaces-as-string
+              string)
+         (str string
+              spaces-as-string))))))
+
+(defn- stringify-todo
+  [{:keys [title added done priority]}]
+  (let [dateline (str (-> (.getYear added) (+ 1900))
+                      "-"
+                      (.getMonth added)
+                      "-"
+                      (.getDate added))]
+    (str (pad-string title 20 false)
+         " | added "
+         (pad-string dateline 10)
+         " | done? " done)))
+
+(defn- numbered-todos
+  []
+  (let [hellos (sort-by :date @state)]
+    (map (fn [a b] [a b])
+         (range 1 (-> (count hellos) inc))
+         (map stringify-todo hellos))))
+
+(defn write-todos!
   [screen]
   (let [print-cmd (map #(s/put-string screen
                                       1
                                       (first %)
                                       (second %))
-                       (sorted-input-items))]
-    (s/clear screen)
-    (dorun print-cmd)
-    (s/redraw screen)))
+                       (numbered-todos))]
+    (dorun print-cmd)))
 
-(comment
-  (s/redraw scr)
-  (s/clear scr)
-  (print-todos! scr))
-
-(defn clear-print
-  [screen f & args]
+(defn refresh-todos!
+  []
   (s/clear screen)
-  (apply f args)
+  (write-todos! screen)
   (s/redraw screen))
 
 (defn main
-  [args])
+  [& args]
+  (s/start screen)
+  (refresh-todos!))
 
 (defn -main
   [& args]
